89fb45c26e31e0dd8aa3191af9484cfa37978c7e
Fomel4ik <63006043+Fomel4ik@users.noreply.github.com>
2022-05-12 17:08:40 +0700

    Create virtual_painter.py

---
+++ virtual_painter.py
@@ -0,0 +1,45 @@
+
+from sre_constants import BRANCH
+import FingerTrackingModule as ftm
+import cv2
+import os
+import numpy as np
+import time
+
+DEBUG = True
+FOLDER_PATH = 'Header'
+listHeader = os.listdir(FOLDER_PATH)
+if DEBUG:
+    print(listHeader)
+
+BRUSH_THICKNESS = 25
+ERASER_THICKNESS = 25
+DRAW = False
+
+drawColor = np.zeros(1, np.uint8)
+
+
+cap = cv2.VideoCapture(0)
+width = 1920
+height = 1080
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+cv2.namedWindow('Virtual Painter', cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty('Virtual Painter', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+prevTime = time.time()
+fps = 0
+while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print('Не удалось получить кадр с web-камеры')
+            continue
+        image = cv2.flip(image, 1)
+        if DEBUG:
+            cv2.putText(image, f'FPS: {int(fps)}', (20,50) ,cv2.FONT_HERSHEY_PLAIN, 3,(0,100,250), 5 )
+        cv2.imshow('Virtual Painter', image)
+        currentTime = time.time()
+        fps = 1/ (currentTime - prevTime)
+        prevTime = currentTime
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
